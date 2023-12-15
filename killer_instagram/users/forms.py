23aee82735bc4e1f0fbc762851d7b2914370ad64
diixo <anatolii.podoba@gmail.com>
2023-12-15 16:23:29 +0200

    fixed small: after logout fix

--- killer_instagram/users/forms.py
+++ killer_instagram/users/forms.py
@@ -4,10 +4,9 @@ from django.forms import CharField, TextInput, EmailField, EmailInput, PasswordI
 
 class RegisterForm(UserCreationForm):
     username   = CharField(max_length=100, required=True, widget=TextInput(attrs={"class": "form-control"}))
-    first_name = CharField(max_length=150, widget=TextInput(attrs={"class": "form-control"}))
-    last_name  = CharField(max_length=150, widget=TextInput(attrs={"class": "form-control"}))
-    email      = EmailField(max_length=150, required=True, 
-                            widget=EmailInput(attrs={"class": "form-control"}))
+    first_name = CharField(max_length=150, required=False, widget=TextInput(attrs={"class": "form-control"}))
+    last_name  = CharField(max_length=150, required=False, widget=TextInput(attrs={"class": "form-control"}))
+    email      = EmailField(max_length=150, required=True, widget=EmailInput(attrs={"class": "form-control"}))
     password1  = CharField(max_length=15, min_length=8, required=True, 
                            widget=PasswordInput(attrs={"class": "form-control"}))
     password2  = CharField(max_length=15, min_length=8, required=True, 
--- killer_instagram/users/urls.py
+++ killer_instagram/users/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from django.urls import path
 
 from .forms import LoginForm
 from .views import RegisterView
@@ -11,7 +10,7 @@ app_name = "users"
 urlpatterns = [
 
     path("signup/", RegisterView.as_view(), name="register"),
-    path("signin/", LoginView.as_view(template_name="users/signin.html",
-                                      authentication_form=LoginForm), name="login"),
+    path("signin/", LoginView.as_view(template_name="users/signin.html", authentication_form=LoginForm, 
+	                                    redirect_authenticated_user=True), name="login"),
     path("logout/", LogoutView.as_view(template_name="users/logout.html"), name="logout")
 ]
